a995d9fdca45520fa49ad31208270ec37b4f8409
= <=>
2018-07-23 13:48:51 -0400

    Added Class Name as output

--- controllerParser.py
+++ controllerParser.py
@@ -2,19 +2,23 @@ import os
 import re
 
 def main():
-  rootdir = 'path to folder here'
+  rootdir = 'ENTER PATH HERE'
   for root, subFolders, files in os.walk(rootdir):
     for filename in files:
       fname = os.path.join(root,filename)
       with open(fname) as f:
         for line in f:
+          if line.startswith("class"):
+            className = line.split()[1]
           if 'public function __construct(' in line:
             pass
           elif 'public function' in line:
             if '$' in line:
               controller,parameters = controllerParser(line)
+              print "Class Name: %s"%(className)
               print "Controller: %s"%(controller)
               print "Parameter(s): %s"%(parameters)
+              print '\n'
           else:
             pass
 
